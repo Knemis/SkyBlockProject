5c4f71ca765d3054e60091b941a9db646ab17a30
knemis <alc13no@gmail.com>
2025-06-03 15:22:23 +0300

    a

---
+++ src/main/java/com/knemis/skyblock/skyblockcoreproject/secondcore/multiversion/v1_20_R1/multiversion/IridiumInventory_V1_20_R1.java
@@ -0,0 +1,12 @@
+package com.knemis.skyblock.skyblockcoreproject.secondcore.multiversion.v1_20_R1.multiversion;
+
+import com.knemis.skyblock.skyblockcoreproject.secondcore.multiversion.common.multiversion.SkyBlockProjectInventory;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class SkyBlockProjectInventory_V1_20_R1 extends SkyBlockProjectInventory {
+    @Override
+    public Inventory getTopInventory(Player player) {
+        return player.getOpenInventory().getTopInventory();
+    }
+}
